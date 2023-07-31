a80949e382d8903bf369d32192c9c45b780b2c15
samarpith1 <arpith.samuel@pragtech.co.in>
2023-07-31 12:04:58 +0530

    [add] manifest

--- students/__manifest__.py
+++ students/__manifest__.py
@@ -7,7 +7,7 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Long description of module's purpose. Cool Students
     """,
 
     'author': "My Company",
