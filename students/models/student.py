511b03a43b1ecb62dc51f71e40f3e03560434340
samarpith1 <arpith.samuel@pragtech.co.in>
2023-07-31 11:31:53 +0530

    [add] new code

---
+++ students/models/student.py
@@ -0,0 +1,13 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+class StudentDetail(models.Model):
+    _name = 'student.student'
+    _description = 'Student Description'
+
+    name  = fields.Char(string='Student Name',required=True)
+    age = fields.Integer(string='Age')
+    class_name = fields.Char(string='Class Name')
+    
